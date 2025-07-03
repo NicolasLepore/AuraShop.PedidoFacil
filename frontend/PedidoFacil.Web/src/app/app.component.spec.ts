7c72c1eb1edf90fc28ec3ba95eda3dba2d717b1e
NicolasLepore <nicolaslpr06@gmail.com>
2025-07-02 21:37:04 -0300

    Arrumando erros - Tela de Login responsiva

--- frontend/PedidoFacil.Web/src/app/app.component.spec.ts
+++ frontend/PedidoFacil.Web/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AuraShop.PedidoFacil.Frontend' title`, () => {
+  it(`should have the 'AuraShop.PedidoFacil.Web' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AuraShop.PedidoFacil.Frontend');
+    expect(app.title).toEqual('AuraShop.PedidoFacil.Web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AuraShop.PedidoFacil.Frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AuraShop.PedidoFacil.Web');
   });
 });
--- frontend/PedidoFacil.Web/src/app/app.routes.ts
+++ frontend/PedidoFacil.Web/src/app/app.routes.ts
@@ -1,19 +1,3 @@
 import { Routes } from '@angular/router';
-import { FormComponent } from './components/form/form.component';
-import { SuccessComponent } from './components/success/success.component';
-import { ItemsComponent } from './components/items/items.component';
 
-export const routes: Routes = [
-    {
-        path: "",
-        component: FormComponent 
-    },
-    {
-        path: "success",
-        component: SuccessComponent
-    },
-    {
-        path: "items",
-        component: ItemsComponent
-    }
-];
+export const routes: Routes = [];
